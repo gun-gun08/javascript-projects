8f5785ffc99470d730ec51cea360f0a30426e9c6
gungun <mahajangungun97@gmail.com>
2025-03-18 21:32:46 +0530

    Initial commit

---
+++ Password generator/script.js
@@ -0,0 +1,20 @@
+function generatePassword() {
+    const length = 12; // Password length
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+
+    document.getElementById("password").value = password;
+}
+
+// Copy to clipboard
+document.getElementById("copyBtn").addEventListener("click", function () {
+    const passwordField = document.getElementById("password");
+    passwordField.select();
+    document.execCommand("copy");
+    alert("Password Copied!");
+});

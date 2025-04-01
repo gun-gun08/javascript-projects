11d47ee4f1e77d28f4dab0eaa7b6ce6902f58e79
gungun <mahajangungun97@gmail.com>
2025-04-01 11:43:35 +0530

    Initial commit

---
+++ Toast Notification/script.js
@@ -0,0 +1,12 @@
+let toastBox = document.getElementById('toastBox');
+
+function showToast(message, type) {
+    let toast = document.createElement('div');
+    toast.classList.add('toast', type);
+    toast.innerHTML = message;
+    toastBox.appendChild(toast);
+
+    setTimeout(() => {
+        toast.remove();
+    }, 3000);
+}
